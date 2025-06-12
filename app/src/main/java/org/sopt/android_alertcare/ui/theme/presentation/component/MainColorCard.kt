c107d36676e73cba39d2ca85b99da4906f4214d7
Hyoeun <joohyoeun@sookmyung.ac.kr>
2025-06-12 16:03:48 +0900

    [feat/#2] 컴포넌트 제작

---
+++ app/src/main/java/org/sopt/android_alertcare/ui/theme/presentation/component/MainColorCard.kt
@@ -0,0 +1,54 @@
+package org.sopt.android_alertcare.ui.theme.presentation.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+
+@Composable
+fun MainColorCard(
+    text: String,
+    textColor: Color = Color.Black,
+    roundShape: Int = 20,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(
+                color = Color(0xFFC9E9FF),
+                shape = RoundedCornerShape(roundShape)
+            )
+            .padding(vertical = 4.dp)
+            .padding(start = 12.dp),
+        horizontalArrangement = Arrangement.Start,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = text,
+            color = textColor,
+            fontSize = 14.sp,
+            fontWeight = FontWeight.Bold
+        )
+
+    }
+}
+
+@Preview
+@Composable
+private fun PreviewScreen() {
+    MainAgeCard("87")
+}
+
