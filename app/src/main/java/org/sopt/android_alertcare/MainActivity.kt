1a62f17640565bc196931e71fab9b443a13cb256
Hyoeun <joohyoeun@sookmyung.ac.kr>
2025-06-18 02:55:15 +0900

    [feat/#2] 네비게이션 구현

--- app/src/main/java/org/sopt/android_alertcare/MainActivity.kt
+++ app/src/main/java/org/sopt/android_alertcare/MainActivity.kt
@@ -4,12 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
 import org.sopt.android_alertcare.ui.theme.AndroidAlertCareTheme
-import org.sopt.android_alertcare.ui.theme.presentation.LoginScreen
+import org.sopt.android_alertcare.ui.theme.presentation.navigation.MainNavHost
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,24 +14,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AndroidAlertCareTheme {
-                LoginScreen()
+                val navController = rememberNavController()
+                MainNavHost(
+                    navController = navController,
+                )
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AndroidAlertCareTheme {
-        Greeting("Android")
     }
 }
