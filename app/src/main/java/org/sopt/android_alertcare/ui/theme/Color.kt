8395c1f2be93cb33b4e6f68d5071216ee06b3f7d
Hyoeun <joohyoeun@sookmyung.ac.kr>
2025-07-31 03:03:20 +0900

    [feat/#5] 컬러 세팅

--- app/src/main/java/org/sopt/android_alertcare/ui/theme/Color.kt
+++ app/src/main/java/org/sopt/android_alertcare/ui/theme/Color.kt
@@ -9,3 +9,7 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val BaseOrange = Color(0xFFFEC59A)    // #fec59a
+val Orange = Color(0xFFFF9F5D)        // #ff9f5d
+val MainOrange = Color(0xFFFD6C41)    // #fd6c41
