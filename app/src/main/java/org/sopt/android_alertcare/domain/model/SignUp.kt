d9818e2dc62a083c4a1e59c7767d32b8f958630f
Hyoeun <joohyoeun@sookmyung.ac.kr>
2025-07-31 10:02:01 +0900

    [feat/#7] Model 구현

---
+++ app/src/main/java/org/sopt/android_alertcare/domain/model/SignUp.kt
@@ -0,0 +1,8 @@
+package org.sopt.android_alertcare.domain.model
+
+data class SignUp (
+    val careGiverName: String,
+    val careReceiverAge: Int,
+    val careReceiverName: String,
+    val careReceiverPhoneNumber: String
+)
