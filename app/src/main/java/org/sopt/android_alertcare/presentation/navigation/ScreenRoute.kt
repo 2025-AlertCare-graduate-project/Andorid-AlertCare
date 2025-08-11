ddb95f3cdb173ce570440a74665d83eaf2d2f479
Hyoeun <joohyoeun@sookmyung.ac.kr>
2025-08-12 02:54:56 +0900

    [mod/#18] 화면 이동 추가 구현

--- app/src/main/java/org/sopt/android_alertcare/presentation/navigation/ScreenRoute.kt
+++ app/src/main/java/org/sopt/android_alertcare/presentation/navigation/ScreenRoute.kt
@@ -2,11 +2,14 @@ package org.sopt.android_alertcare.presentation.navigation
 
 
 object ScreenRoute {
-    const val LOGIN_SCREEN = "login_screen"
-    const val LOGIN_COMPLETE_SCREEN = "login_complete_screen"
+    const val SIGN_UP_SCREEN = "sign_up_screen"
+    const val SIGN_UP_COMPLETE_SCREEN = "sign_up_complete_screen"
 
     const val MAIN_SCREEN = "main_screen"
     const val VIDEO_SCREEN = "video_screen/{videoId}"
+    const val LOGIN_SCREEN = "login_screen"
+    const val LOGIN_COMPLETE_SCREEN = "login_complete_screen"
 
+    const val SPLASH_SCREEN = "splash_screen"
 
 }
