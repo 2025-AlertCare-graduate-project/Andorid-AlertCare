61aa242339e2d2566a0fa567995e543d8c78d77a
Hyoeun <joohyoeun@sookmyung.ac.kr>
2025-08-27 01:39:38 +0900

    [mod/#22] 형식 변경

--- app/src/main/java/org/sopt/android_alertcare/domain/repository/SignUpRepository.kt
+++ app/src/main/java/org/sopt/android_alertcare/domain/repository/SignUpRepository.kt
@@ -4,12 +4,13 @@ import org.sopt.android_alertcare.domain.model.LogIn
 import org.sopt.android_alertcare.domain.model.SignUp
 import org.sopt.android_alertcare.domain.model.SignUpResponse
 import org.sopt.android_alertcare.domain.model.VideoCheck
+import org.sopt.android_alertcare.domain.model.VideoDetail
 import org.sopt.android_alertcare.domain.model.VideoList
 
 interface SignUpRepository {
     suspend fun signUp(signUp: SignUp): Result<SignUpResponse>
     suspend fun videoList(phoneNumber: String): Result<List<VideoList>>
-    suspend fun videoDetail(id: Long): Result<String>
+    suspend fun videoDetail(id: Long): Result<VideoDetail>
     suspend fun logIn(logIn: LogIn): Result<SignUpResponse>
     suspend fun videoChecked(id: Long):Result<VideoCheck>
 
